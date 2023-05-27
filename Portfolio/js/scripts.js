1768c0bfa24e7968256b313edaadc5d7b9b4b504
Bilawal Khan <131751844+EngrBilawalKhan@users.noreply.github.com>
2023-05-27 11:37:00 +0500

    Add files via upload

---
+++ Portfolio/js/scripts.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ["Graphic Designer", "Web Developer", "YouTube Creator"],
+    typeSpeed: 40,
+    backSpeed: 100,
+    loop: true
+};
+
+var typed = new Typed('.auto-input', options);
